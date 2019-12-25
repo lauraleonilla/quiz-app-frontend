2d2767e3c05632997772123d2486f0512e6f083f
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2019-12-25 11:07:05 +0100

    Rendering first question and buttons

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CONSTANTS from '../constants'
+import { Button } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 const QuizPage = props => {
   console.log('Quiz props', props)
+  const [currentQuestion, setcurrentQuestion] = useState(0)
+
+  const renderQuestions = () => {
+    return props.selectedQuiz[currentQuestion].question
+  }
+
+  const clickHandler = () => {
+    console.log('lol')
+  }
+
   return (
     <div>
-         Here will be qustion
+      {renderQuestions()}
+      {props.selectedQuiz[currentQuestion].type === CONSTANTS.BOOLEAN ? (
+        <div>
+          <Button basic color='purple' content='True' onClick={() => clickHandler()} />
+          <Button basic color='purple' content='False' onClick={() => clickHandler()} />
+        </div>)
+        : null}
     </div>
   )
 }
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export default {
   GOT_USER: 'GOT_USER',
   QUIZ_DATA: 'QUIZ_DATA',
-  SELECT_QUIZ: 'SELECT_QUIZ'
+  SELECT_QUIZ: 'SELECT_QUIZ',
+  BOOLEAN: 'boolean'
 }
