f0658a69e6aea3ee2a6d300a593969061efcb671
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2020-01-04 16:34:24 +0100

    Refactored file structure, fixed routing on quiz page

--- src/initialState.js
+++ src/initialState.js
@@ -2,7 +2,6 @@ export default {
   appState: {
     user: null,
     quizData: [],
-    filmQuizData: [],
     selectedQuiz: null,
     currentScore: 0
   }
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
-import QuizContainer from './components/QuizContainer'
+import QuizContainer from './components/quizComponents/QuizContainer'
 import Login from './components/Login'
 import Header from './components/Header'
 import Chat from './components/Chat'
-import ProfilePage from './components/ProfilePage'
-import QuizPage from './components/QuizPage'
+import ProfilePage from './components/profileComponents/ProfilePage'
+import QuizPage from './components/quizComponents/QuizPage'
 import NotFoundPage from './components/NotFoundPage'
 import Register from './components/Register'
 
-const Routes = props => {
+const Routes = () => {
   return (
     <div>
       <Router>
@@ -23,13 +22,11 @@ const Routes = props => {
           <Route exact path='/login' render={() => <Login />} />
           <Route exact path='/register' render={() => <Register />} />
           <Route exact path='/profile' render={() => <ProfilePage />} />
-          {props.selectedQuiz && props.selectedQuiz.length ? (
-            <Route
-              exact
-              path={`/quiz/${props.selectedQuiz[0].category}`}
-              render={() => <QuizPage />}
-            />
-          ) : null}
+          <Route
+            exact
+            path={'/quiz/:selectedQuiz'}
+            render={() => <QuizPage />}
+          />
           <Route component={NotFoundPage} />
         </Switch>
       </Router>
@@ -37,8 +34,4 @@ const Routes = props => {
   )
 }
 
-const mapStateToProps = state => ({
-  selectedQuiz: state.appState.selectedQuiz
-})
-
-export default connect(mapStateToProps, null)(Routes)
+export default Routes
